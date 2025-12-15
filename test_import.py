ba73c67196f24b3cd92ba65c43197dc4880c9b26
Sanjay7773 <149701770+Sanjay7773@users.noreply.github.com>
2025-12-15 14:27:37 +0530

    k

--- test_import.py
+++ test_import.py
@@ -1,6 +1,9 @@
+from SmartApi.smartApiWebsocket import SmartWebSocket
+
+
 # from SmartApi.smartWebSocketV1 import SmartWebSocket
 # from SmartApi.smartWebsocket import SmartWebSocket
-from SmartApi.webSocket import WebSocket
+# from SmartApi.webSocket import WebSocket
 
 
 print("Imported OK")
--- websocket_v1.py
+++ websocket_v1.py
@@ -4,9 +4,11 @@
 # ------------------------------------------------------------
 
 from SmartApi import SmartConnect
+from SmartApi.smartApiWebsocket import SmartWebSocket
+
 # from SmartApi.smartWebSocketV1 import SmartWebSocket
-# from SmartApi.smartWebsocket import SmartWebSocket
-from SmartApi.webSocket import WebSocket
+#from SmartApi.smartWebsocket import SmartWebSocket
+# from SmartApi.webSocket import WebSocket
 
 
 
